72e09f6ae5f4ff6fcc7785b3b6de83470b4a8805
MattFofo <matteo.segattini2@gmail.com>
2022-05-02 15:29:45 +0200

    getters e setters

--- Product/toy.php
+++ Product/toy.php
@@ -13,5 +13,65 @@
             $this->price = $_price,
          ];
       }
+
+      /**
+       * Get the value of animalType
+       */ 
+      public function getAnimalType()
+      {
+            return $this->animalType;
+      }
+
+      /**
+       * Set the value of animalType
+       *
+       * @return  self
+       */ 
+      public function setAnimalType($animalType)
+      {
+            $this->animalType = $animalType;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of expiring
+       */ 
+      public function getExpiring()
+      {
+            return $this->expiring;
+      }
+
+      /**
+       * Set the value of expiring
+       *
+       * @return  self
+       */ 
+      public function setExpiring($expiring)
+      {
+            $this->expiring = $expiring;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of description
+       */ 
+      public function getDescription()
+      {
+            return $this->description;
+      }
+
+      /**
+       * Set the value of description
+       *
+       * @return  self
+       */ 
+      public function setDescription($description)
+      {
+            $this->description = $description;
+
+            return $this;
+      }
    }
 ?>
